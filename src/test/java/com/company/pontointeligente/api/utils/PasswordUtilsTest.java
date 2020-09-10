27f0c6d0dbf8e330f916b57bf9ee7b4c5f7c8ade
Ramon Cunha <ramon.network@hotmail.com>
2020-09-10 18:45:03 -0300

    PasswordUtil test

--- src/test/java/com/company/pontointeligente/api/utils/PasswordUtilsTest.java
+++ src/test/java/com/company/pontointeligente/api/utils/PasswordUtilsTest.java
@@ -1,4 +1,24 @@
 package com.company.pontointeligente.api.utils;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordUtilsTest {
+
+    private static final String SENHA = "123456";
+    private final BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+
+    @Test
+    public void testSenhaNula() throws Exception {
+        Assertions.assertNull(PasswordUtils.gerarBCrypt(null));
+    }
+
+    @Test
+    public void testGerarHashSenha() throws Exception {
+        String hash = PasswordUtils.gerarBCrypt(SENHA);
+
+        Assertions.assertTrue(bCryptEncoder.matches(SENHA, hash));
+    }
+
 }
