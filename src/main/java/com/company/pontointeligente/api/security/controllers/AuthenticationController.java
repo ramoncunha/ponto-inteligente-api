44b5ba1c4a1bba78f49318b17232a9db4713407e
Ramon Cunha <ramon.network@hotmail.com>
2020-09-17 19:23:39 -0300

    Finalizando AuthenticationController

--- src/main/java/com/company/pontointeligente/api/security/controllers/AuthenticationController.java
+++ src/main/java/com/company/pontointeligente/api/security/controllers/AuthenticationController.java
@@ -13,12 +13,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -39,6 +38,7 @@ public class AuthenticationController {
         this.userDetailsService = userDetailsService;
     }
 
+    @PostMapping
     public ResponseEntity<Response<TokenDto>> gerarTokenJwt(@Valid @RequestBody JwtAuthenticationDto authenticationDto,
                                                             BindingResult result) {
         Response<TokenDto> response = new Response<>();
@@ -57,6 +57,33 @@ public class AuthenticationController {
         UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationDto.getEmail());
         String token = jwtTokenUtil.obterToken(userDetails);
         response.setData(new TokenDto(token));
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = "/refresh")
+    public ResponseEntity<Response<TokenDto>> gerarRefreshTokenJwt(HttpServletRequest request) {
+        log.info("Gerando refresh token JWT.");
+        Response<TokenDto> response = new Response<>();
+
+        Optional<String> token = Optional.ofNullable(request.getHeader(TOKEN_HEADER));
+
+        if(token.isPresent() && token.get().startsWith(BEARER_PREFIX)) {
+            token = Optional.of(token.get().substring(7));
+        }
+
+        if(!token.isPresent()) {
+            response.getErrors().add("Token não informado");
+        } else if (!jwtTokenUtil.tokenValido(token.get())) {
+            response.getErrors().add("Token inválido ou expirado.");
+        }
+
+        if(!response.getErrors().isEmpty()) {
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        String refreshedToken = jwtTokenUtil.refreshToken(token.get());
+        response.setData(new TokenDto(refreshedToken));
 
         return ResponseEntity.ok(response);
     }
